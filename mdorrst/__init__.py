91ebca9a8d649f49efd4d106a2e1cb693ba56f4c
Julien Palard <julien@palard.fr>
2017-04-27 22:40:53 +0200

    Bump version: 0.3.0 → 0.4.0

--- mdorrst/__init__.py
+++ mdorrst/__init__.py
@@ -4,6 +4,6 @@ from __future__ import absolute_import
 
 __author__ = """Julien Palard"""
 __email__ = 'julien@palard.fr'
-__version__ = '0.3.0'
+__version__ = '0.4.0'
 
 from mdorrst.mdorrst import sniff
