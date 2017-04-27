de0178c4211b3cfa59acd0d926caaee5f57489f0
Julien Palard <julien@palard.fr>
2017-04-27 22:40:53 +0200

    Bump version: 0.3.0 → 0.4.0

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ test_requirements = [
 
 setup(
     name='mdorrst',
-    version='0.3.0',
+    version='0.4.0',
     description="Tell appart Markdown and reStructuredText.",
     long_description=readme + '\n\n' + history,
     author="Julien Palard",
