575fd358f81cc336e1c95bb76470613991593912
Julien Palard <julien@palard.fr>
2017-04-27 14:01:12 +0200

    Python 2 compatibility for pypa/pypi-legacy.

--- mdorrst/__init__.py
+++ mdorrst/__init__.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
+from __future__ import absolute_import
+
 __author__ = """Julien Palard"""
 __email__ = 'julien@palard.fr'
 __version__ = '0.2.0'
--- mdorrst/mdorrst.py
+++ mdorrst/mdorrst.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+from __future__ import print_function, division, absolute_import
+
 import re
 from mdorrst import __version__
 
